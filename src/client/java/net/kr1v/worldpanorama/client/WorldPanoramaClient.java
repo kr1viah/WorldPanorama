5fac628991a0531893858c749d7f5088cd6e0489
esoij <Dariuspotup@gmail.com>
2026-06-08 14:47:54 -0600

    cleanup code
    general cleanup

--- src/client/java/net/kr1v/worldpanorama/client/WorldPanoramaClient.java
+++ src/client/java/net/kr1v/worldpanorama/client/WorldPanoramaClient.java
@@ -1,14 +1,7 @@
 package net.kr1v.worldpanorama.client;
 
-import net.fabricmc.api.ClientModInitializer;
-
-public class WorldPanoramaClient implements ClientModInitializer {
+public class WorldPanoramaClient {
 	public static boolean isLoadingPanoramaWorld;
 	public static boolean isInTitleScreen;
 	public static boolean hasTitleScreenOpen;
-
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
 }
